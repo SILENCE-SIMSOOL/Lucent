e058306ff65e768381e9c60b0305869fc1f2d0f9
SILENCE-SIMSOOL <79349144+SILENCE-SIMSOOL@users.noreply.github.com>
2026-07-19 09:05:11 +0900

    keybind round

--- src/main/java/silence/simsool/lucent/Lucent.java
+++ src/main/java/silence/simsool/lucent/Lucent.java
@@ -46,7 +46,7 @@ public class Lucent implements ClientModInitializer {
 
 	public static final String ID = "lucent";
 	public static final String NAME = "Lucent";
-	public static final String VERSION = "1.3.3";
+	public static final String VERSION = "1.3.4";
 	public static String LATEST_VERSION = "Fetching...";
 
 	public static Minecraft mc = Minecraft.getInstance();
